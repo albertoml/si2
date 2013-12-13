25fdd3d270bc4828f5769f6debb40b02da79c6f0
Alberto Martinez <albertoml.ohara@gmail.com>
2013-12-13 12:12:08 +0100

    Reutilizado y copiado las clases al buscador buscar como leer foto

---
+++ BuscadorCaras/src/buscadorcaras/ClasFuerte.java
@@ -0,0 +1,168 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package buscadorcaras;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author alberto
+ */
+public class ClasFuerte {
+    
+    public ArrayList<Hiperplano> Mejores;
+    private int iteraciones;
+    
+    public ClasFuerte(int i){
+        
+        iteraciones= i;
+    }
+    
+    public float ObtenerC(Hiperplano h, Cara c){
+        
+        float sol=0;
+        int [] auxPunto= c.getData();
+        float [] auxVector= h.getVector();
+        
+        for(int i=0; i<auxPunto.length; i++){
+            
+            sol+= auxPunto[i]*auxVector[i];
+        }
+        
+        return sol;
+    }
+    
+    public boolean Test (float v, Cara c){
+        
+        boolean sol=false;
+        
+        //si es mayor que 0 y es cara acierto
+        //si es menor que 0 y es no cara acierto
+        //si es mayor que 0 y no cara fallo
+        //si es menor que 0 y cara fallo
+        if(v > 0){
+            if(c.getTipo()==1){
+                    
+                sol=true;
+            }
+        }
+        else{
+            if(c.getTipo()==-1){
+                    
+                sol=true;
+            }
+        }
+        
+        return sol;
+    }
+    
+    public Hiperplano Entrenar(int clasificadores, ArrayList<Cara> aprender, int max[], 
+            int min[], double peso[]){
+        
+        float varC, resta;
+        double tasaError, tasaMenor=1;
+        
+        Hiperplano solucion = new Hiperplano();
+        ArrayList<Hiperplano> Hiperplanos = new ArrayList();
+        for(int i=0; i<clasificadores; i++){
+            
+            Hiperplanos.add(new Hiperplano(max, min));
+        }
+        
+        for (int i=0; i<Hiperplanos.size(); i++){
+            
+            //probar hiperplano uno a uno
+            tasaError=0;
+            for(int j=0; j<aprender.size(); j++){
+                
+                varC=ObtenerC(Hiperplanos.get(i), aprender.get(j));
+                resta=varC - Hiperplanos.get(i).getC();
+                
+                if(!Test(resta, aprender.get(j))){
+                    
+                    tasaError+= peso[j];
+                }
+            }
+            Hiperplanos.get(i).setTasaError(tasaError);
+            if(tasaError<tasaMenor){
+                
+                tasaMenor=tasaError;
+                solucion=Hiperplanos.get(i);
+            }
+        }
+        
+        return solucion;
+    }
+    
+    public ArrayList<Hiperplano> Adaboost(ArrayList<Cara> listaAprendizaje, int clasificadores, 
+            int[] max, int[] min){
+        
+        Hiperplano ht= new Hiperplano();
+        ArrayList<Hiperplano> listaEntrenados = new ArrayList();
+        double clasificacion=0;
+        //vector de pesos
+        double [] Peso = new double[listaAprendizaje.size()];
+        int tamaño= listaAprendizaje.size();
+        //inicializo el vector para normalizarlo entre su tamaño
+        for(int i=0; i<tamaño; i++){
+            
+            Peso[i]= (float) 1/tamaño;
+        }
+        
+        for(int i=0; i<iteraciones; i++){
+            
+            //entrenamos el clasificador debil con la tasa de error
+            ht=Entrenar(clasificadores, listaAprendizaje, max, min, Peso);
+            //con la tasa de error calculamos alfa
+            double alfa= 1/2 * (Math.log((1-ht.getTasaError())/ht.getTasaError()));
+            ht.setAlfa(alfa);
+            listaEntrenados.add(ht);
+            //acualizamos el vector de pesos
+            float varC, resta;
+            double suma=0;
+            int j;
+            for(j=0; j<tamaño-1; j++){
+                
+                varC=ObtenerC(ht, listaAprendizaje.get(j));
+                resta=varC - ht.getC();
+                if(Test(resta, listaAprendizaje.get(j))){
+                    //clasifica bien
+                    Peso[j+1]= Peso[j] * Math.pow(Math.E, (alfa));
+                }
+                else{
+                    //clasifica mal
+                    Peso[j+1]= Peso[j] * Math.pow(Math.E, (-alfa));
+                }
+                suma+=Peso[j];
+            }
+            suma+=Peso[j];
+            //normalizamos el vector de pesos
+            for(int k=0;k<tamaño; k++){
+                
+                Peso[k]= (Peso[k]/suma);
+            }
+            //actualizamos el clasificador fuerte
+            for(int k=0; k<listaEntrenados.size(); k++){
+                
+                listaEntrenados.get(k).SobreEntrenar(Peso, listaAprendizaje);
+            }     
+            //calcular condicion y devolver
+            if(ht.getTasaError()==0){
+                
+                Mejores= listaEntrenados;
+                return listaEntrenados;
+            }
+        }
+        Mejores= listaEntrenados;
+        
+        for(int i=0;i<Mejores.size();i++){
+            
+            Mejores.get(i).Testear(listaAprendizaje);
+        }
+        
+        return listaEntrenados;
+    }
+    
+    
+}
---
+++ BuscadorCaras/src/buscadorcaras/Hiperplano.java
@@ -0,0 +1,206 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package buscadorcaras;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author alberto
+ */
+public class Hiperplano {
+    
+    public int []Punto;
+    public float []Vector;
+    public double TasaError;
+    public double alfa;
+    public float C;
+    private static int Dimension=576;
+    private static float MaxVector=1;
+    private static float MinVector=-1;
+    
+    public Hiperplano(){
+        
+        int i;
+        Punto= new int[Dimension];
+        Vector= new float[Dimension];
+        TasaError=0;
+        for (i=0;i<Dimension;i++){
+            
+            Punto[i]=(int)(Math.random()*255);
+            Vector[i]=(float)(Math.random()*(MaxVector-MinVector))+MinVector;    
+        }
+        NormalizarVector();
+        CalcularC();
+    }
+    
+    public Hiperplano(int MaxPunto[], int MinPunto[]){
+        
+        int i;
+        Punto= new int[Dimension];
+        Vector= new float[Dimension];
+        for (i=0;i<Dimension;i++){
+            
+            Punto[i]=(int)(Math.random()*(MaxPunto[i]-MinPunto[i]))+MinPunto[i];
+            Vector[i]=(float)(Math.random()*(MaxVector-MinVector))+MinVector;    
+        }
+        NormalizarVector();
+        CalcularC();
+    }
+    
+    public void CalcularC(){
+        
+        float sum=0;
+        for(int i=0; i<Dimension; i++){
+            
+            sum+= Punto[i]*Vector[i];
+        }
+        C= sum;
+    }
+    
+    public float ObtenerC(Hiperplano h, Cara c){
+        
+        float sol=0;
+        int [] auxPunto= c.getData();
+        float [] auxVector= h.getVector();
+        
+        for(int i=0; i<auxPunto.length; i++){
+            
+            sol+= auxPunto[i]*auxVector[i];
+        }
+        
+        return sol;
+    }
+    
+    public void NormalizarVector(){
+        
+        float sum=0;
+        for(int i=0; i<Dimension; i++){
+            
+            sum+=Vector[i];
+        }
+        for(int i=0; i<Dimension; i++){
+            
+            Vector[i]= Vector[i]/sum;
+        }
+    }
+     public void Testear(ArrayList<Cara> test){
+        
+        int acierto=0, fallo=0;
+        float varC, resta;
+        for(int j=0; j<test.size(); j++){
+                
+            varC=ObtenerC(this, test.get(j));
+            //lo comparamos con la c del hiperplano
+            resta=varC - this.getC();
+            if(resta>0){
+                if(test.get(j).getTipo()==1){
+                    
+                    acierto++;
+                }
+                else{
+                    
+                    fallo++;
+                }
+            }
+            else{
+                if(test.get(j).getTipo()==-1){
+                    
+                    acierto++;
+                }
+                else{
+                    
+                    fallo++;
+                }
+            }
+        }
+        System.out.println("aciertos     fallos    Tasa aciertos");
+        float division;
+            
+        division=(float) acierto/test.size();
+        System.out.println(acierto + "         " + fallo + "        " + division + "%");
+    }
+     
+     public boolean Test (float v, Cara c){
+        
+        boolean sol=false;
+        
+        //si es mayor que 0 y es cara acierto
+        //si es menor que 0 y es no cara acierto
+        //si es mayor que 0 y no cara fallo
+        //si es menor que 0 y cara fallo
+        if(v > 0){
+            if(c.getTipo()==1){
+                    
+                sol=true;
+            }
+        }
+        else{
+            if(c.getTipo()==-1){
+                    
+                sol=true;
+            }
+        }
+        
+        return sol;
+    } 
+    
+    public void SobreEntrenar(double [] Peso, ArrayList<Cara> lista){
+        
+        double tasaError=0, tasaMenor=1;
+        float varC, resta;
+        for(int j=0; j<lista.size(); j++){
+                
+            varC=ObtenerC(this, lista.get(j));
+            resta=varC - this.getC();
+
+            if(!Test(resta, lista.get(j))){
+
+                tasaError+= Peso[j];
+            }
+        }
+        this.setTasaError(tasaError);
+    }
+    
+    public float getC(){
+        
+        return C;
+    }
+    public double getAlfa(){
+        
+        return alfa;
+    }
+    public int[] getPunto(){
+        
+        return Punto;
+    }
+    public float[] getVector(){
+        
+        return Vector;
+    }
+    public double getTasaError(){
+        
+        return TasaError;
+    }
+    public void setC(float c){
+        
+        C=c;
+    }
+    public void setAlfa(double a){
+        
+        alfa=a;
+    }
+    public void setPunto(int[] p){
+        
+        Punto=p;
+    }
+    public void setVector(float[] v){
+        
+        Vector=v;
+    }
+    public void setTasaError(double t){
+        
+        TasaError= t;
+    }
+}
